6b4906b6a0a317919ee43a9b6309c94fc396e9eb
Michael Casadevall <mcasadevall@ubuntu.com>
2010-01-12 19:05:16 -0500

    Forward-port libcamera2 to Eclair.
    Fill in missing defines that were determined via reverse engineering
    
    Signed-off-by: Michael Casadevall <mcasadevall@ubuntu.com>

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@
 #
 
 common_msm_dirs := libcopybit liblights libopencorehw librpc libstagefrighthw
-msm7k_dirs := $(common_msm_dirs) boot libgralloc libaudio
+msm7k_dirs := $(common_msm_dirs) boot libgralloc libaudio libcamera2
 qsd8k_dirs := $(common_msm_dirs) libgralloc-qsd8k libaudio-qsd8k dspcrashd
 
 ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
