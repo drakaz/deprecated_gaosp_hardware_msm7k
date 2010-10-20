drakaz/deprecated_gaosp_hardware_msm7k

commit 78db1d72ef9e5acf20834de39917e5073b1d2e4e

msm7k: Add new device filter for Geeksphone ONE
The GP ONE ("adq" board) carries its own libaudio, gralloc, and camera, so
build just the common msm7k libraries.
Also... Prettified checks a bit, to make them more readable.

Change-Id: Id1c1ea3d6c3b7ae5e4e40b03947cae4dbbaa3bc1

Android.mk
@@ -21,18 +21,19 @@ qsd8k_dirs := $(common_msm_dirs) libgralloc-qsd8k libaudio-qsd8k dspcrashd
 #msm7x30_dirs := $(common_msm_dirs) libgralloc-qsd8k libaudio-msm7x30 liboverlay
 msm7x30_dirs := $(common_msm_dirs) liboverlay libaudio-msm7x30
 
-ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
-  ifeq ($(TARGET_BOARD_PLATFORM_GPU),qcom-adreno200)
+ifeq ($(TARGET_BOARD_PLATFORM_GPU),qcom-adreno200)
+    ### MSM7k with Adreno GPU
     include $(call all-named-subdir-makefiles,$(msm7k_adreno_dirs))
-  else
+else ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),adq)
+    ### "adq" board (7x25)
+    include $(call all-named-subdir-makefiles,$(common_msm_dirs))
+else ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
+    ### MSM7x30
+    include $(call all-named-subdir-makefiles,$(msm7x30_dirs))
+else ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
+    ### Other MSM7k
     include $(call all-named-subdir-makefiles,$(msm7k_dirs))
-  endif
-else
-  ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+else ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+    ### QSD8k
     include $(call all-named-subdir-makefiles,$(qsd8k_dirs))
-  else
-    ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
-      include $(call all-named-subdir-makefiles,$(msm7x30_dirs))
-    endif
-  endif
 endif

commit 99b5ad6788462098696eca2bff07391ae57ca344

msm7k: Fix device selection priority
Commit 78db1d72ef9e5acf20834de39917e5073b1d2e4e broke qsd8k. Oopsie.

Change-Id: I217766c48a4ee922a75ce8da0e11517d0eee505a

Android.mk
@@ -21,19 +21,19 @@ qsd8k_dirs := $(common_msm_dirs) libgralloc-qsd8k libaudio-qsd8k dspcrashd
 #msm7x30_dirs := $(common_msm_dirs) libgralloc-qsd8k libaudio-msm7x30 liboverlay
 msm7x30_dirs := $(common_msm_dirs) liboverlay libaudio-msm7x30
 
-ifeq ($(TARGET_BOARD_PLATFORM_GPU),qcom-adreno200)
-    ### MSM7k with Adreno GPU
-    include $(call all-named-subdir-makefiles,$(msm7k_adreno_dirs))
+ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
+    ### QSD8k
+    include $(call all-named-subdir-makefiles,$(qsd8k_dirs))
 else ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),adq)
     ### "adq" board (7x25)
     include $(call all-named-subdir-makefiles,$(common_msm_dirs))
 else ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
     ### MSM7x30
     include $(call all-named-subdir-makefiles,$(msm7x30_dirs))
+else ifeq ($(TARGET_BOARD_PLATFORM_GPU),qcom-adreno200)
+    ### MSM7k with Adreno GPU
+    include $(call all-named-subdir-makefiles,$(msm7k_adreno_dirs))
 else ifeq ($(TARGET_BOARD_PLATFORM),msm7k)
     ### Other MSM7k
     include $(call all-named-subdir-makefiles,$(msm7k_dirs))
-else ifeq ($(TARGET_BOARD_PLATFORM),qsd8k)
-    ### QSD8k
-    include $(call all-named-subdir-makefiles,$(qsd8k_dirs))
 endif