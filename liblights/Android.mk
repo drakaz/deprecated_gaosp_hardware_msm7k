381d5fd92e73564d5bd94127524561e152635094
Ricardo Cerqueira <github@cerqueira.org>
2010-12-20 00:03:20 +0000

    msm7k: Add the LOCAL_MODULE_TAGS to all modules
    Change-Id: I1b0289988ec664db198a6c4de33e80dd77b8b6f8

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -18,6 +18,7 @@ LOCAL_PATH:= $(call my-dir)
 # hw/<COPYPIX_HARDWARE_MODULE_ID>.<ro.board.platform>.so
 include $(CLEAR_VARS)
 
+LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := lights.c
 
 LOCAL_PRELINK_MODULE := false
