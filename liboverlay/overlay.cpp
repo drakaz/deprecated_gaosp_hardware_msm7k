94dee0bc386856e3dbcc390a655b33d8582ef9b0
Steve Kondik <shade@chemlab.org>
2010-12-22 16:54:54 -0500

    msm7k: Fix typo in overlay->mdp mapping
    Change-Id: I59730b7b2ed51d08999896af816ecc0dabeaf927

--- liboverlay/overlay.cpp
+++ liboverlay/overlay.cpp
@@ -113,7 +113,7 @@ static int get_format(int format) {
 		case OVERLAY_FORMAT_RGB_565:       return MDP_RGB_565;
 		case HAL_PIXEL_FORMAT_YCbCr_422_SP:  return MDP_Y_CBCR_H2V1;
 		case HAL_PIXEL_FORMAT_YCrCb_420_SP:  return MDP_Y_CRCB_H2V2;
-		case HAL_PIXEL_FORMAT_YCbCr_420_SP:  return MDP_Y_CRCB_H2V2;
+		case HAL_PIXEL_FORMAT_YCbCr_420_SP:  return MDP_Y_CBCR_H2V2;
 	}
 	return -1;
 }
